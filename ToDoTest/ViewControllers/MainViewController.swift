3756de0871877177f84d77bcb881d1b032dcd9bf
ArtemPavlov72 <pavlov.tema@gmail.com>
2023-08-07 15:28:36 +0500

    Initial commit. Created ToDo CoreData.

---
+++ ToDoTest/ViewControllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  ToDoTest
+//
+//  Created by Artem Pavlov on 07.08.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+}
+
